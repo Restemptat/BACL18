d8aacaf9e785510931fc276efe6d09861284b6b0
Анастасия Колодяжная <nasty@MacBook-Air-Anastasia.local>
2020-10-24 17:43:59 +0300

    HW2 Done

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -1,3 +1,12 @@
 def solution(arr):
-    ### YOUR CODE ###
-    return
+	l = 1 #curent length of same figures
+	lmax = 1 # max quantity of consecutive elements
+	n = len(arr)
+	for i in range(n):
+		if arr[i] == arr[i-1]:
+			l = l+1
+		else:
+			l = 1
+		if l > lmax:
+			max = l
+	return lmax
