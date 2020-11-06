0de713af100717d96c1d77952e3289d758367203
Анастасия Колодяжная <nasty@MacBook-Air-Anastasia.local>
2020-11-06 22:46:59 +0100

    adding files

--- HW2/A/A.py
+++ HW2/A/A.py
@@ -2,11 +2,14 @@ def solution(arr):
 	l = 1 #curent length of same figures
 	lmax = 1 # max quantity of consecutive elements
 	n = len(arr)
-	for i in range(n):
-		if arr[i] == arr[i-1]:
-			l = l+1
-		else:
-			l = 1
-		if l > lmax:
-			max = l
+	if n == 1:
+		return 1
+	else:
+		for i in range(n-1):
+			if arr[i+1] == arr[i]:
+				l = l+1
+			else:
+				l = 1
+			if l > lmax:
+				lmax = l
 	return lmax
--- HW2/B/B.py
+++ HW2/B/B.py
@@ -1,3 +1,17 @@
 def solution(x):
-    ### YOUR CODE ###
-    return
+    t = ''
+    l = 0
+    for i in range(len(x)):
+        if x[i] == 'h':
+            l+=1
+            if l > 1 :
+                x = x[:i] + 'H' + x[i+1:]
+    for i in range(len(x)-1,0,-1):
+        if x[i] == 'H':
+            x = x[:i] + 'h' + x[i+1:]
+            break
+    for i in range(len(x)):
+        if i % 3 != 0 or i == 0:
+            t = t + x[i]
+    t = t.replace('1','one')
+    return(t)
