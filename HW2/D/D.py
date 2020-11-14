dbe425640755608ce97fdcbf6ba78dd44f2c7a7e
Анастасия Колодяжная <nasty@MacBook-Air-Anastasia.local>
2020-11-14 16:55:03 +0100

    adding other filess

--- HW2/D/D.py
+++ HW2/D/D.py
@@ -1,16 +1,24 @@
 def solution(n,k):
-	arr = list(range(1, n+1))
+    print("Case n=", n, "k=", k)
+    if n == 1:
+        return 1
+    if n == 0:
+        return 0
+    if k == 1:
+        return n
+    arr = list(range(1, n+1))
+    l=0
+    arr1 = list()
+    while len(arr) > 1:
+        for i in range(len(arr)):
+            #print(arr[i])
+            l+=1
+            if l != k:
+                arr1.append(arr[i])
+            else:
+                l = 0
+            #print(arr1, l, arr[i])
+        arr = arr1
+        arr1 = list()		
+    return arr[0]
 
-	#print(arr)
-
-	l=0
-	arr1 = arr
-	while len(arr) > 1:
-		for i in range(len(arr)):
-			l+=1
-			if l == 3:
-				arr1 = arr1[:i]+arr1[i+1:]
-				l=1
-			print(arr1)
-		arr = arr1		
-	return arr[0]
